2f79ef8598a3ea4847a1d84c641eac2cec92a8b2
Raffa Bux <raffaelebussolotto@gmail.com>
2024-05-17 00:40:26 +0200

    project file structure setup

--- src/tests/app.component.spec.ts
+++ src/tests/app.component.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { AppComponent } from '../app/components/default/app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
