21ea8beed71772b5185cb700b785ac66e7333e4f
RaffaBux <raffaelebussolotto@gmail.com>
2025-01-21 17:52:54 +0100

    repository update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,10 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-
 export class AppComponent {
-  title = 'spencer';
+  title = 'Spencer';
 }
